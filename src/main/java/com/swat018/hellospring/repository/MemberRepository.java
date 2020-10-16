d2821202e414c0807002a2230cd4740ca80506e5
jinwoo park <swat018@gmail.com>
2020-10-17 00:31:36 +0900

    회원 관리 예제 (백엔드 개발) - 회원 도메인과 리포지토리 만들기

---
+++ src/main/java/com/swat018/hellospring/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package com.swat018.hellospring.repository;
+
+import com.swat018.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
