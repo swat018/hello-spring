f4acfb7e9c87dc6f1e49b092d21ded238e0ca0fc
jinwoo park <swat018@gmail.com>
2020-10-31 22:50:19 +0900

    스프링 DB 접근 기술 - 스프링 데이터 JPA

--- src/main/java/com/swat018/hellospring/SpringConfig.java
+++ src/main/java/com/swat018/hellospring/SpringConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 
 import javax.persistence.EntityManager;
 import javax.sql.DataSource;
+import java.lang.reflect.Member;
 
 /**
  * @author jinwoopark
@@ -15,12 +16,12 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-    private EntityManager em;
+/*    private EntityManager em;
 
     @Autowired
     public SpringConfig(EntityManager em) {
         this.em = em;
-    }
+    }*/
 
 /*    private DataSource dataSource;
 
@@ -29,16 +30,24 @@ public class SpringConfig {
         this.dataSource = dataSource;
     }*/
 
-    @Bean
-    public MemberService memberService() {
-        return new MemberService(memberRepository());
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
+    }
+
+/*    @Bean
     public MemberRepository memberRepository() {
  //       return new MemoryMemberRepository();
  //       return new JdbcMemberRepository(dataSource);
  //       return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+ //       return new JpaMemberRepository(em);
+
+    }*/
 }
