b1a0f9e97e25368e055da101ea9ae1c3f0d2749c
jinwoo park <swat018@gmail.com>
2020-11-01 10:33:29 +0900

    AOP - AOP가 필요한 상황

--- src/main/java/com/swat018/hellospring/service/MemberService.java
+++ src/main/java/com/swat018/hellospring/service/MemberService.java
@@ -29,10 +29,19 @@ public class MemberService {
      * @return
      */
     public Long join(Member member) {
+
+        long start = System.currentTimeMillis();
+
         // 같은 이름이 있는 중복 회원X (중복 회원 검증)
-        validateDuplicateMember(member);
-        memberRepository.save(member);
-        return member.getId();
+        try {
+            validateDuplicateMember(member);
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = " + timeMs + "ms");
+        }
     }
 
     private void validateDuplicateMember(Member member) {
@@ -47,7 +56,14 @@ public class MemberService {
      * @return
      */
     public List<Member> findMembers() {
-        return memberRepository.findAll();
+        long start = System.currentTimeMillis();
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers " + timeMs + "ms");
+        }
     }
 
     public Optional<Member> findOne(Long memberId) {
